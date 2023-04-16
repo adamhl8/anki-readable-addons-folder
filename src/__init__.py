e9ff796338700bdb39e43eb151a7a2a843a700cd
Adam Langbert <adamhl@pm.me>
2023-04-15 21:49:46 -0500

    improve symlink creation on windows

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import json
+from os import system
 from pathlib import Path
 
+from anki.utils import is_win
 from aqt import mw
 
 if mw is None:
@@ -12,11 +14,12 @@ if config is None:
     message = "config is None"
     raise RuntimeError(message)
 
-anki_addons_path = Path(mw.pm.addonFolder())
+anki_addons_path = Path(mw.pm.addonFolder()).resolve(strict=True)
 folder_name = config.get("folderName") or "Readable Addons"
-readable_addons_path = (anki_addons_path.parent / folder_name).resolve()
+readable_addons_path = anki_addons_path.parent / folder_name
 readable_addons_path.mkdir(exist_ok=True)
 
+windows_command = ""
 installed_addons: set[str] = set()
 for addon_dir in anki_addons_path.iterdir():
     if not addon_dir.is_dir():
@@ -34,7 +37,14 @@ for addon_dir in anki_addons_path.iterdir():
     installed_addons.add(addon_name)
     readable_addon_dir = readable_addons_path / addon_name
     if not readable_addon_dir.exists():
-        readable_addon_dir.symlink_to(addon_dir, target_is_directory=True)
+        if is_win:
+            windows_command += f'mklink /J "{readable_addon_dir.resolve()}" "{addon_dir.resolve()}" && '
+        else:
+            readable_addon_dir.symlink_to(addon_dir, target_is_directory=True)
+
+if is_win and windows_command:
+    windows_command = windows_command.removesuffix(" && ")
+    system(windows_command)
 
 addon_symlinks: set[str] = set()
 for addon_symlink in readable_addons_path.iterdir():
